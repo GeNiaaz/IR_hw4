2e229bc900a0f72d12f4570d23f5ae37fdb9ee8d
rabhijit <40291462+rabhijit@users.noreply.github.com>
2021-04-20 19:50:33 +0800

    refactored search.py

--- search.py
+++ search.py
@@ -148,6 +148,7 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     reg_exp = " |;|&|<|>|\(|\)|\[|\]|\{|\}|\"|\n|\'|\.\.\.|\+|\:|\;|\!|\?| $| #|% "
     # dict to cache retrieved word data from pointers
     word_data = {}
+    relevant_docs = []
 
     print('loading files from disk...')
     # unpickle and load dictionary from file
@@ -157,93 +158,89 @@ def run_search(dict_file, postings_file, queries_file, results_file):
 
     print('finding matching documents...')
     # open query file, postings and results file
-    with open(queries_file) as query_file, gzip.open(postings_file, "rb") as post_file, open(results_file, "w") as output_file:
-        line_count = 0
-        # for each query:
-        for line in query_file:
-            # instantiate dict to store tf and idf for each term in query
-            query = {}
-            # initialize dict to keep score for each document
-            scores = {}
+    with open(queries_file) as query_file:
+        content = query_file.readlines()
+        query_string = content[0]
+        for i in range(1, len(content)):
+            relevant_docs.append(content[i].strip())
 
-            # for each word in query:
-            for word in re.split(reg_exp, line):
-                # strip starting and ending punctuation marks e.g. quotes, full stops, ellipses from each word
-                if not all(letter in string.punctuation for letter in word):
-                    while word[-1] in string.punctuation:
-                        word = word[:-1]
-                    while word[0] in string.punctuation:
-                        word = word[1:]
-                    # case-fold and stem word
-                    word = Porter.stem(word.lower())
-                    # add word to query data dict, with term freq. of 0, if not yet seen in query
-                    if word not in query.keys():
-                        query[word] = 0
-                    # increment term freq. of word in query
-                    query[word] += 1
+    # initialize query dict
+    query = {}
+    # initialize dict to keep score for each document
+    scores = {}
+    with gzip.open(postings_file, "rb") as post_file:
+        # for each word in query:
+        for word in re.split(reg_exp, query_string):
+            # strip starting and ending punctuation marks e.g. quotes, full stops, ellipses from each word
+            if not all(letter in string.punctuation for letter in word):
+                while word[-1] in string.punctuation:
+                    word = word[:-1]
+                while word[0] in string.punctuation:
+                    word = word[1:]
+                # case-fold and stem word
+                word = Porter.stem(word.lower())
+                # add word to query data dict, with term freq. of 0, if not yet seen in query
+                if word not in query.keys():
+                    query[word] = 0
+                # increment term freq. of word in query
+                query[word] += 1
 
-                    """
-                    retrieve postings lists for each word in query, using pointers stored in dictionary,
-                    if word has not already been seen before, and store in word_data
-                    """
-                    if word not in word_data.keys():
-                        if word in dictionary:
-                            post_file.seek(dictionary[word][1])
-                            idf = dictionary[word][0]
-                            postings = pickle.loads(post_file.read(dictionary[word][2]))
-                            # store postings and idf value for word in word_data
-                            word_data[word] = [postings, idf]
-                        else:
-                            # if word is not in doc. collection, initialize empty postings list, and idf val. of 0
-                            word_data[word] = [{}, 0]
+                """
+                retrieve postings lists for each word in query, using pointers stored in dictionary,
+                if word not already been seen before in query, and store in word_data
+                """
+                if word not in word_data.keys():
+                    if word in dictionary:
+                        post_file.seek(dictionary[word][1])
+                        idf = dictionary[word][0]
+                        postings = pickle.loads(post_file.read(dictionary[word][2]))
+                        # store postings and idf value for word in word_data
+                        word_data[word] = [postings, idf]
+                        # calculate non-normalized score for word in query
+                        query[word] = idf * (1 + math.log(query[word], 10))
+                    else:
+                        # if word is not in doc. collection, initialize empty postings list, and idf val. of 0
+                        word_data[word] = [{}, 0]
 
-                    idf = word_data[word][1]
-                    # calculate non-normalized score for word in query
-                    query[word] = idf * (1 + math.log(query[word], 10))
+    # after iterating over query, compute total query vector length, to be used in normalization
+    query_total = math.sqrt(sum(value ** 2 for value in query.values()))
 
-            # after iterating over query, compute total query vector length, to be used in normalization
-            query_total = math.sqrt(sum(value ** 2 for value in query.values()))
-
-            # for each word in collected query data:
-            for word in query:
-                postings = word_data[word][0]
-                # for each doc in word's postings list:
-                for doc in postings:
-                    # initialize document in score vector if not yet seen, with score of 0
-                    if doc not in scores.keys():
-                        scores[doc] = 0
-                    """
-                    multiply document score for this word (pre-computed and normalized during indexing)
-                    with query score for this word (which is divided by total query vector length for normalization),
-                    to get total weight for this word. add this weight to the score for this document.
-                    """
-                    scores[doc] += (query[word] / query_total) * postings[doc]["tf"]
-
+    # for each word in collected query data:
+    for word in query:
+        postings = word_data[word][0]
+        # for each doc in word's postings list:
+        for doc in postings:
+            # initialize document in score vector if not yet seen, with score of 0
+            if doc not in scores.keys():
+                scores[doc] = 0
             """
-            after query is iterated over entirely, construct min heap out of score dictionary,
-            with -score as the primary node value. Effectively a max heap, but making all scores
-            negative allows us to select documents by smallest docId first as a secondary
-            deciding value, if two nodes have the same score.
+            multiply document score for this word (pre-computed and normalized during indexing)
+            with query score for this word (which is divided by total query vector length for normalization),
+            to get total weight for this word. add this weight to the score for this document.
             """
-            heap = [(-value, key) for key, value in scores.items()]
+            scores[doc] += (query[word] / query_total) * postings[doc]["tf"]
 
-            with open(results_file, 'a') as output_file:
-                write_string = ""
-                value_count = 0
-                # select max. 10 docs with the 'smallest' (i.e. most negative) scores
-                # if two docs have the same score, doc. with smaller docId value will be selected first
-                for value in heapq.nsmallest(10, heap):
-                    # write to output file
-                    write_string += (str(value[1]))
-                    value_count += 1
-                    # insert space if value is not last in heap
-                    if value_count != min(len(heap), 10):
-                        write_string += " "
-                line_count += 1
-                # insert newline before writing line to file, if line is not the first
-                if line_count != 1:
-                    output_file.write("\n")
-                output_file.write(write_string)
+    """
+    after query is iterated over entirely, construct min heap out of score dictionary,
+    with -score as the primary node value. Effectively a max heap, but making all scores
+    negative allows us to select documents by smallest docId first as a secondary
+    deciding value, if two nodes have the same score.
+    """
+    heap = [(-value, key) for key, value in scores.items() if value != 0]
+
+    with open(results_file, 'w') as output_file:
+        write_string = ""
+        value_count = 0
+        # select max. 10 docs with the 'smallest' (i.e. most negative) scores
+        # if two docs have the same score, doc. with smaller docId value will be selected first
+        for value in heapq.nsmallest(len(heap), heap):
+            # write to output file
+            write_string += (str(value[1]))
+            value_count += 1
+            # insert space if value is not last in heap
+            if value_count != min(len(heap), 10):
+                write_string += " "
+        output_file.write(write_string)
 
     end_time = time.time()
     print("done.")
@@ -253,7 +250,7 @@ def run_search(dict_file, postings_file, queries_file, results_file):
 # This is to allow us to export search.py as a library
 # Otherwise, whenever `search.py` is exported, we will execute the following lines of code.
 # See `search-test.py` for import use.
-if __name__ == "main":
+if __name__ == "__main__":
     dictionary_file = postings_file = file_of_queries = output_file_of_results = None
 
     try:
