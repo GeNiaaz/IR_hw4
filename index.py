3335c377143ede455ed8bb880d26aea165f5776a
rabhijit <40291462+rabhijit@users.noreply.github.com>
2021-04-21 16:47:12 +0800

    added vector space model indexing/searching

--- index.py
+++ index.py
@@ -1,142 +1,79 @@
-#!/usr/bin/python3
-
-import re
-import nltk
 import sys
 import getopt
-# ------------
-import os
 import csv
 import gzip
-import string
 import pickle
-import math
+import joblib
+import nltk
+import numpy as np
+import Stemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+stemmer = Stemmer.Stemmer("english")
+class TfidfStemVectorizer(TfidfVectorizer):
+    def __init__(self):
+        super().__init__(self, dtype=np.float32)
+        self.analyze = super().build_analyzer()
+    def build_analyzer(self):
+        return lambda doc: (stemmer.stemWord(word) for word in self.analyze(doc))
 
 def usage():
-    print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+    print("usage: " + sys.argv[0] + " -i dataset-file -d dictionary-file -p postings-file")
 
-"""
-Function to iterate through each file in a folder in ascending file-name order.
-"""
-def filename_sort(filename):
-    number_regex = re.compile(r'(\d+)')
-    parts = number_regex.split(filename)
-    parts[1::2] = map(int, parts[1::2])
-    return parts
-
-"""
-build index from documents stored in the input directory,
-then output the dictionary file and postings file
-"""
 def build_index(in_dir, out_dict, out_postings):
     warning_check = input("are you sure you want to overwrite your index? y/n ")
     if warning_check != "y":
         print("closing...")
         sys.exit(2)
 
-    Porter = nltk.stem.PorterStemmer()
-    """
-    filter out all punctuation except for hyphens, forward slashes, dots and commas,
-    as they are necessary for numbers, dates and phrases
-    """
-    reg_exp = " |;|&|<|>|\(|\)|`|\[|\]|\{|\}|\"|\n|\'|\.\.\.|\+|\:|\;|\!|\?| $| #|% "
-    # dictionary to store term frequency and postings data
-    word_data = {}
-    # dictionary to collect document lengths
-    documents = {}
+    corpus = []
+    docs = []
+    positions = {}
+    vectorizer = TfidfStemVectorizer()
 
-    print('processing dataset...')
-    # iterate through and open each file in input dir., in ascending order
+    print("processing dataset...")
     with open(in_dir) as dataset:
         csv.field_size_limit(sys.maxsize)
         dataset_reader = csv.reader(dataset, delimiter=",")
-        line_count = 0
-        next(dataset)  # skip header line
+        column_no = 1
+        next(dataset) # skip header line
         for line in dataset_reader:
-            print("\tline", line_count); line_count += 1
+            print("\treading line", column_no); column_no += 1
             doc_id = line[0]
             title = line[1]
             text = line[2]
             date = line[3]
             court = line[4]
 
-            # instantiate document in documents dict with a length of 0
-            documents[doc_id] = 0
-            # read all text from file
-            #file_content = file.read()
+            position_count = 0
+            for word in vectorizer.build_analyzer()(text):
+                if word not in positions:
+                    positions[word] = {}
+                if doc_id not in positions[word]:
+                    positions[word][doc_id] = []
+                positions[word][doc_id].append(position_count)
+                position_count += 1
 
-            # filter text using regex, and iterate through each word in text
-            filtered_text = re.split(reg_exp, text)
-            for word in filtered_text:
-                original_word = word
-                # ignore word if it only consists of punctuation marks e.g. "..." or "--"
-                if not all(letter in string.punctuation for letter in word):
-                    # since ending full-stops and commas are not sieved out by our regex, we remove them here
-                    while word[-1] in string.punctuation:
-                        word = word[:-1]
-                    # case-fold and stem each token
-                    word = Porter.stem(word.lower())
-                    # instantiate word in word_data if word is not already seen before
-                    if word not in word_data.keys():
-                        # each word in word_data is linked to a postings dict with docId as key and term freq as value
-                        word_data[word] = {}
+            docs.append(doc_id)
+            corpus.append(text)
+        dataset.close()
 
-                    posting = word_data[word]
-                    # add current document to current word's postings if not yet added
-                    if doc_id not in posting.keys():
-                        posting[doc_id] = {"tf": 0}
-                        posting[doc_id]["positions"] = [index for index, term in enumerate(filtered_text) if term == original_word]
+    print("building vector space matrix...")
+    matrix = (vectorizer.fit_transform(corpus))
+    dictionary = vectorizer.vocabulary_
 
-                    # increment term frequency for this document, for this word
-                    posting[doc_id]["tf"] += 1
-
-    # delete old dictionary and postings files if they still exist
-    for file in [out_dict, out_postings]:
-        if os.path.exists(file):
-            os.remove(file)
-
-    # instantiate dictionary which will be used to store terms and pointers to their postings lists later
-    dictionary = {}
-    # get doc. collection size, to compute idf
-    collection_size = len(documents)
-
-    print('building vector space matrix...')
-    # for each word seen in doc. collection
-    for word in word_data.keys():
-        posting = word_data[word]
-        # replace the tf values for each doc. the word is seen in, with (1 + log(tf)) values
-        for doc in posting.keys():
-            posting[doc]["tf"] = 1 + math.log(posting[doc]["tf"], 10)
-            # add square of (1 + log(tf)) value to that document's length in dictionary
-            # to be used in normalization later
-            documents[doc] += posting[doc]["tf"] ** 2
-
-    print('calculating tf-idf scores...')
-    with gzip.open(out_postings, 'ab') as postings_file:
-        # for each word in word_data
-        for word in word_data.keys():
-            posting = word_data[word]
-            # for each document in word's posting list
-            for doc in posting.keys():
-                # normalize (1 + log(tf)) value for each doc by dividing with document length collected earlier
-                posting[doc]["tf"] /= math.sqrt(documents[doc])
-
-            # calculate idf, and store idf and pointer to term data for each term in the dictionary
-            dictionary[word] = [math.log(collection_size / len(posting.keys()), 10), postings_file.tell()]
-            # pickle and dump term data to postings file
-            postings_file.write(pickle.dumps(posting))
-            # store how many bytes of data to read from initial pointer value
-            dictionary[word].append(postings_file.tell() - dictionary[word][1])
-
-    print('saving to disk...')
-    # dump dictionary to dictionary file
-    with gzip.open(out_dict, 'wb') as dictionary_file:
-        pickle.dump(dictionary, dictionary_file)
+    print("saving to disk...")
+    with gzip.open(out_postings, 'wb') as post_file:
+        joblib.dump(matrix, post_file, protocol=pickle.HIGHEST_PROTOCOL)
+    with gzip.open(out_dict, 'wb') as dict_file:
+        joblib.dump(vectorizer, dict_file, protocol=pickle.HIGHEST_PROTOCOL)
+    with gzip.open('docs.txt', 'wb') as doc_file:
+        joblib.dump(docs, doc_file, protocol=pickle.HIGHEST_PROTOCOL)
+    with gzip.open('positions.txt', 'wb') as posn_file:
+        joblib.dump(positions, posn_file, protocol=pickle.HIGHEST_PROTOCOL)
 
     print("done.")
-    print(out_dict, "and", out_postings, "generated.")
-
-
+    #print(vectorizer.get_feature_names())
 
 input_directory = output_file_dictionary = output_file_postings = None
 
@@ -147,11 +84,11 @@ except getopt.GetoptError:
     sys.exit(2)
 
 for o, a in opts:
-    if o == '-i': # input directory
+    if o == '-i':  # input directory
         input_directory = a
-    elif o == '-d': # dictionary file
+    elif o == '-d':  # dictionary file
         output_file_dictionary = a
-    elif o == '-p': # postings file
+    elif o == '-p':  # postings file
         output_file_postings = a
     else:
         assert False, "unhandled option"
--- search.py
+++ search.py
@@ -1,17 +1,23 @@
-#!/usr/bin/python3
-
-import re
-import nltk
 import sys
-import getopt
-# ------------
-import heapq
-import gzip
-import string
-import pickle
-import math
 import time
+import getopt
+import csv
+import gzip
+import joblib
+import nltk
 import numpy as np
+import Stemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+stemmer = Stemmer.Stemmer("english")
+class TfidfStemVectorizer(TfidfVectorizer):
+    def __init__(self):
+        super().__init__(self, dtype=np.float32, ngram_range=(1, 3))
+        self.analyze = super().build_analyzer()
+
+    def build_analyzer(self):
+        return lambda doc: (stemmer.stemWord(word) for word in self.analyze(doc))
 
 def usage():
     print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
@@ -30,215 +36,45 @@ def rocchio_calculation(alpha, beta, query_vec, *doc_vecs):
     final_result_list = final_result.tolist()
     return final_result_list
 
-
-def parse_query_file(query_f):
-    # Vars to return
-    query = ""
-    list_of_relevance = []
-
-    # Parsing query file
-    with open(query_f, 'r') as query_readable:
-        raw_input = []
-        for line in query_readable:
-            raw_input.append(line.strip())
-
-        query = parse_query(raw_input[0])
-        for a in range(1, len(raw_input)):
-            list_of_relevance.append(raw_input[a])
-
-    return query, list_of_relevance
-
-
-def parse_query(s):
-    queries = s.split(" AND ")
-    return queries
-
-# '"i am a phrase"' (note the additional single quotes surrounding the string.)
-def is_phrasal(s):
-    return len(s) > 1 and s[0] == '"'
-
-# "hello world"
-def is_bog(s):
-    return not is_phrasal(s)
-
-# "hello world" -> ["hello", "world"]
-def words_bog(bog):
-    return bog.split()
-
-def process_AND(list_a, list_b):
-    ptr_a = 0
-    ptr_b = 0
-
-    max_index_a = len(list_a)
-    max_index_b = len(list_b)
-
-    resultant_list = []
-
-    while ptr_a < max_index_a and ptr_b < max_index_b:
-        curr_a = list_a[ptr_a]
-        curr_b = list_b[ptr_b]
-
-        if curr_a == curr_b:
-            ptr_a += 1
-            ptr_b += 1
-
-            resultant_list.append(curr_a)
-        else:
-            if curr_a < curr_b:
-                ptr_a += 1
-            else:
-                ptr_b += 1
-
-    return resultant_list
-
-postings_cache = {} # Consider caching our pickle readings for postings 
-
-def find_docs_for_phrasal_query(query, dictionary):
-    # Assume query comes with the quotation marks, i.e. => "hello there world" 
-    query_words = query.split()
-    
-    # Find docs containing all query words
-    idf_query_words = [(dictionary[word][0], word) for word in query_words]
-    idf_query_words = set(idf_query_words) # remove duplicates   
-    sorted_idf_query_words = sorted(idf_query_words, reverse=True)   # sort query_words by highest idf (appears in least docs)
-
-    merged_docs = set()
-
-    for (idf, word) in sorted_idf_query_words:
-        if word not in postings_cache: # cache operation
-            # post_file.seek(dictionary[word][1])
-            # postings_cache[word] = pickle.loads(post_file.read(dictionary[word][2]))
-            None
-        else:
-            if (merged_docs == set()): # set merged_docs to the first set of doc_ids
-                # merged_docs = set(postings_cache[word].keys())
-                None
-            else: # find intersection of doc_ids sets, in increasing order of size (since sorted by highest idf) (faster merging)
-                # merged_docs = merged_docs.intersection(set(postings_cache[word].keys()))
-                None
-
-    # Find docs with the exact phrase 
-    result_docs = []
-
-    for doc_id in merged_docs:
-        merged_postitions = set()
-        for (idx, word) in enumerate(query_words):
-            positions = [(position - idx) for position in postings_cache[word][doc_id][1]] # Adjusting the positional index to that of the first query word
-            if (merged_postitions == set()):
-                merged_postitions = set(positions)
-            else:
-                merged_postitions = merged_postitions.intersection(set(positions))
-        
-        if merged_postitions != set():  # Consider adding weights too => larger set == more frequently-appearing phrase == more relevant
-            result_docs.append(doc_id) 
-
-    return result_docs
-
 def run_search(dict_file, postings_file, queries_file, results_file):
-    """
-    using the given dictionary file and postings file,
-    perform searching on the given queries file and output the results to a file
-    """
     start_time = time.time()
-    Porter = nltk.stem.PorterStemmer()
-    """
-    filter out all punctuation except for hyphens, forward slashes, dots and commas,
-    as they are necessary for numbers, dates and phrases
-    """
-    reg_exp = " |;|&|<|>|\(|\)|\[|\]|\{|\}|\"|\n|\'|\.\.\.|\+|\:|\;|\!|\?| $| #|% "
-    # dict to cache retrieved word data from pointers
-    word_data = {}
-    relevant_docs = []
 
-    print('loading files from disk...')
-    # unpickle and load dictionary from file
-    with gzip.open(dict_file, "rb") as dictionary_file:
-        dictionary = pickle.load(dictionary_file)
-    dictionary_file.close()
+    print("loading files from disk...")
+    with gzip.open(postings_file, 'rb') as post_file:
+        matrix = joblib.load(post_file)
+    with gzip.open(dict_file, 'rb') as dict_file:
+        vectorizer = joblib.load(dict_file)
+    with gzip.open('docs.txt', 'rb') as doc_file:
+        docs = joblib.load(doc_file)
+    """
+    with gzip.open('positions.txt', 'rb') as posn_file:
+        positions = joblib.load(posn_file)
+    """
 
-    print('finding matching documents...')
-    # open query file, postings and results file
+    print("retrieving query...")
+    relevant_cols = []
     with open(queries_file) as query_file:
         content = query_file.readlines()
-        query_string = content[0]
+        query = content[0]
         for i in range(1, len(content)):
-            relevant_docs.append(content[i].strip())
+            relevant_cols.append(docs.index(content[i].strip()))
 
-    # initialize query dict
-    query = {}
-    # initialize dict to keep score for each document
-    scores = {}
-    with gzip.open(postings_file, "rb") as post_file:
-        # for each word in query:
-        for word in re.split(reg_exp, query_string):
-            # strip starting and ending punctuation marks e.g. quotes, full stops, ellipses from each word
-            if not all(letter in string.punctuation for letter in word):
-                while word[-1] in string.punctuation:
-                    word = word[:-1]
-                while word[0] in string.punctuation:
-                    word = word[1:]
-                # case-fold and stem word
-                word = Porter.stem(word.lower())
-                # add word to query data dict, with term freq. of 0, if not yet seen in query
-                if word not in query.keys():
-                    query[word] = 0
-                # increment term freq. of word in query
-                query[word] += 1
+    print("constructing query vector...")
+    query_vector = vectorizer.transform([query])
+    print("finding matching documents...")
+    cosine_similarities = cosine_similarity(query_vector, matrix).flatten()
+    sorted_cosines = cosine_similarities.argsort()[::-1]
+    result_docs = [docs[doc] for doc in sorted_cosines if cosine_similarities[doc] != 0]
+    print(len(result_docs), "matching documents found.")
 
-                """
-                retrieve postings lists for each word in query, using pointers stored in dictionary,
-                if word not already been seen before in query, and store in word_data
-                """
-                if word not in word_data.keys():
-                    if word in dictionary:
-                        post_file.seek(dictionary[word][1])
-                        idf = dictionary[word][0]
-                        postings = pickle.loads(post_file.read(dictionary[word][2]))
-                        # store postings and idf value for word in word_data
-                        word_data[word] = [postings, idf]
-                        # calculate non-normalized score for word in query
-                        query[word] = idf * (1 + math.log(query[word], 10))
-                    else:
-                        # if word is not in doc. collection, initialize empty postings list, and idf val. of 0
-                        word_data[word] = [{}, 0]
-
-    # after iterating over query, compute total query vector length, to be used in normalization
-    query_total = math.sqrt(sum(value ** 2 for value in query.values()))
-
-    # for each word in collected query data:
-    for word in query:
-        postings = word_data[word][0]
-        # for each doc in word's postings list:
-        for doc in postings:
-            # initialize document in score vector if not yet seen, with score of 0
-            if doc not in scores.keys():
-                scores[doc] = 0
-            """
-            multiply document score for this word (pre-computed and normalized during indexing)
-            with query score for this word (which is divided by total query vector length for normalization),
-            to get total weight for this word. add this weight to the score for this document.
-            """
-            scores[doc] += (query[word] / query_total) * postings[doc]["tf"]
-
-    """
-    after query is iterated over entirely, construct min heap out of score dictionary,
-    with -score as the primary node value. Effectively a max heap, but making all scores
-    negative allows us to select documents by smallest docId first as a secondary
-    deciding value, if two nodes have the same score.
-    """
-    heap = [(-value, key) for key, value in scores.items() if value != 0]
-
+    print("writing to file...")
     with open(results_file, 'w') as output_file:
         write_string = ""
-        value_count = 0
-        # select max. 10 docs with the 'smallest' (i.e. most negative) scores
-        # if two docs have the same score, doc. with smaller docId value will be selected first
-        for value in heapq.nsmallest(len(heap), heap):
-            # write to output file
-            write_string += (str(value[1]))
-            value_count += 1
-            # insert space if value is not last in heap
-            if value_count != min(len(heap), 10):
+        doc_count = 0
+        for doc in result_docs:
+            doc_count += 1
+            write_string += doc
+            if doc_count != len(result_docs):
                 write_string += " "
         output_file.write(write_string)
 
@@ -246,33 +82,31 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     print("done.")
     print(results_file, "generated.")
     print("search completed in", round(end_time - start_time, 5), "secs.")
+    #print(matrix.shape)
 
-# This is to allow us to export search.py as a library
-# Otherwise, whenever `search.py` is exported, we will execute the following lines of code.
-# See `search-test.py` for import use.
-if __name__ == "__main__":
-    dictionary_file = postings_file = file_of_queries = output_file_of_results = None
+dictionary_file = postings_file = file_of_queries = output_file_of_results = None
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
 
-    for o, a in opts:
-        if o == '-d':
-            dictionary_file = a
-        elif o == '-p':
-            postings_file = a
-        elif o == '-q':
-            file_of_queries = a
-        elif o == '-o':
-            file_of_output = a
-        else:
-            assert False, "unhandled option"
+for o, a in opts:
+    if o == '-d':
+        dictionary_file = a
+    elif o == '-p':
+        postings_file = a
+    elif o == '-q':
+        file_of_queries = a
+    elif o == '-o':
+        file_of_output = a
+    else:
+        assert False, "unhandled option"
 
-    if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None:
-        usage()
-        sys.exit(2)
+if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None:
+    usage()
+    sys.exit(2)
 
-    run_search(dictionary_file, postings_file, file_of_queries, file_of_output)
+run_search(dictionary_file, postings_file, file_of_queries, file_of_output)
+
